d1de3d36bc9296ca1f1e0e44ba25e28b4d2149e2
Laercio Simoes <laercio@hpcbrasil.com>
2013-07-31 18:06:53 +0000

    Final Script

--- Karatsuba.py
+++ Karatsuba.py
@@ -1,3 +1,5 @@
+# Sample script to use Karatsuba Algorithm
+# Author: Laercio Simoes
 
 def getHalfSize(x,y):
 	iMax = max(len(str(x)),len(str(y)))
@@ -9,14 +11,17 @@ def karatsuba(x, y):
 	x2 = x - (x1 * pow(10,iHalf))
 	y1 = y / pow(10, iHalf)
 	y2 = y - (y1 * pow(10,iHalf))
-	print "Val1 %s => %s - %s " %(x,x1,x2) 
-	print "Val2 %s => %s - %s " %(y,y1,y2)
-	#x1 = getHalf(x)
-	#x2 = x - x1
-	
 
-	#y1 = getHalf(y)
-	#y2 = y - y1
+	z0 = x2 * y2
+	z1 = (x2 * y1) + (x1 * y2)
+	z2 = x1 * y1
+
+	result = (z2 *  pow(pow(10, iHalf),2)) + (z1 * pow(10, iHalf)) + z0
+
+	print "Val X %s => %s - %s " %(x,x1,x2) 
+	print "Val Y %s => %s - %s " %(y,y1,y2)
+	print "Val Z0  = %s Z1 = %s Z2 = %s " %(z0,z1,z2)
+	print "Result %s " %(result)
 	
 
 def main():
